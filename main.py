78a5c1f74daa49563d3dc88f7ee259d0f9303a54
Gustav Sandström <gussan@omegapoint.se>
2019-03-15 18:10:29 +0100

    removed stupid change to all.json and print every character
    connected to database. credentials in other file not checked in
    
    connected to database. credentials in other file not checked in

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import json
 import os.path
+import psycopg2
+
+
 path = "./api/all.json"
+endpoint = "bigdatalabstoresource.cbjor7hidfop.eu-west-1.rds.amazonaws.com"
+port = 5432
+
+
+
+with open("./credentials/postgres") as credentials:
+    content = credentials.readlines()
+content = [x.strip() for x in content]
+conn = psycopg2.connect(host=content[0], port=port, database=content[1], user=content[2], password=content[3])
+
+print(conn)
+
 
 if os.path.isfile(path):
     print("found file: " + path)
 
 with open(path) as json_file:
     json_data = json.load(json_file)
-    print(json_data)
+    for id in json_data:
+        print(id)
